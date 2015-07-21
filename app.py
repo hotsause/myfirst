ba07c4d23e0225cafff982dfbb28c910c9adefe2
hotsause <loneli.monki@gmail.com>
2015-07-21 23:34:00 +0800

    some text

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ class hello():
     # except:
         # print(sys.exc_info())#################################older
 class Myapp(web.application):
-    def run(self,port=int(os.environ.get('PORT', 8080)),*filllater):#########os.environ.get(...),ensure your application makes use of the port assigned to the user environment,on heroku:fill port=int(os.environ.get(...)) instead
+    def run(self,port=int(os.environ.get('PORT', 8080)),*filllater):#########os.environ.get(...),ensure your application makes use of the port assigned to the user environment,on heroku:fill port=int(os.environ.get(...)) instead for heroku dynamic assigned port every start
         func = self.wsgifunc(*filllater)##########must have ?
         # print(port)###debug
         return web.httpserver.runsimple(func,('0.0.0.0',port))#####wsgifunc is must filled for runsimple()
