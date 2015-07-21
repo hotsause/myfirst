0fb75c91e9b25f5a6233fc92488ab328a75d1561
hotsause <qnmnp-github@yahoo.com>
2015-07-21 17:59:45 +0800

    mod port

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import web
 import sys
+import os
 urls = ('/','hello')
+# global port
+# port = os.environ.get('PORT', 8080)
 # webapp = web.application(urls, globals())
 class hello():
     def GET(self):
         return '<html>\n<body>\n<h1>Hello,there.</h1>\n</body>\n</html>'
+# if __name__ == '__main__':###################################older 
+    # try:
+        # webapp = web.application(urls, globals())
+        # webapp.run()
+    # except:
+        # print(sys.exc_info())#################################older
 class Myapp(web.application):
-    def run(self,port=80,*filllater):
+    def run(self,port=os.environ.get('PORT', 8080),*filllater):#########os.environ.get(...),nsure your application makes use of the port assigned to the user environment
         func = self.wsgifunc(*filllater)##########must have ?
         return web.httpserver.runsimple(func,('0.0.0.0',port))#####wsgifunc is must filled for runsimple()
 if __name__ == '__main__':
     try:
         webapp = Myapp(urls, globals())
-        webapp.run(port=8888)
+        webapp.run()
     except:
-        print(sys.exc_info())
+        print(sys.exc_info())        
