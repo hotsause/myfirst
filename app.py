7a7f880093c2a225bfa3d0b6a2f5d9fa3b0d3101
hotsause <qnmnp-github@yahoo.com>
2015-07-21 17:10:09 +0800

    mod

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ urls = ('/','hello')
 class hello():
     def GET(self):
         return '<html>\n<body>\n<h1>Hello,there.</h1>\n</body>\n</html>'
+class Myapp(web.application):
+    def run(self,port=80,*filllater):
+        func = self.wsgifunc(*filllater)##########must have ?
+        return web.httpserver.runsimple(func,('0.0.0.0',port))#####wsgifunc is must filled for runsimple()
 if __name__ == '__main__':
     try:
-        webapp = web.application(urls, globals())
-        webapp.run()
+        webapp = Myapp(urls, globals())
+        webapp.run(port=8888)
     except:
         print(sys.exc_info())
