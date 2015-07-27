9febec507ca3b2a407bb6219a97167ac825a92d6
hotsause <qnmnp-github@yahoo.com>
2015-07-27 17:43:12 +0800

    return response

--- cla.py
+++ cla.py
@@ -5,7 +5,7 @@ import web
 import os
 # import urllib2
 class Myapp(web.application):
-    def run(self,port=int(os.environ.get('PORT', 8080)),*filllater):#########os.environ.get(...),ensure your application makes use of the port assigned to the user environment,on heroku:fill port=int(os.environ.get(...)) instead for heroku dynamic assigned port every start
+    def run(self,port=int(os.environ.get('PORT', 80)),*filllater):#########os.environ.get(...),ensure your application makes use of the port assigned to the user environment,on heroku:fill port=int(os.environ.get(...)) instead for heroku dynamic assigned port every start
         func = self.wsgifunc(*filllater)##########must have ?
         # print(port)###debug
         return web.httpserver.runsimple(func,('0.0.0.0',port))#####wsgifunc is must filled for runsimple()
--- form.py
+++ form.py
@@ -1,14 +1,27 @@
 import web
 import cla
+import httplib
+import sys
 # import os
 urls = (
         '','fun'
         # '/(.*)','fun'
         )
+HC = httplib.HTTPConnection        
 class fun():
     def GET(self):
       if web.input():
-        return str(web.input().first),str(web.input().second)
+        addr = str(web.input().addr)
+        try:
+            conn = HC(addr,timeout=100)
+            conn.request('get','/')
+            resp = conn.getresponse()
+            return 'header is %s \n\n\n\nresponse is \n%s' % (resp.getheaders(), resp.read())
+        except:    
+            return sys.exc_info()
+        finally:
+            conn.close()
+        # return str(web.input().addr)
       else:
         return 'None'
 pageform = cla.Myapp(urls,locals())      
