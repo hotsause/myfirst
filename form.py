8c8582695eb2f59aa67b411a79c047d93175d9cd
hotsause <qnmnp-github@yahoo.com>
2015-07-28 11:10:16 +0800

    header:ua added

--- form.py
+++ form.py
@@ -8,16 +8,17 @@ urls = (
         '','fun'
         # '/(.*)','fun'
         )
-HC = httplib.HTTPConnection        
+HC = httplib.HTTPConnection     
+heaD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'}   
 class fun():
     def GET(self):
       if web.input():
         addr = str(web.input().addr)
         try:
             conn = HC(addr,timeout=100)
-            conn.request('get','/')
+            conn.request('get','/',headers = heaD)
             resp = conn.getresponse()
-            return 'header is %s \n\n\n\nresponse is \n%s' % (resp.getheaders(), resp.read())
+            return 'header is %s \n\n\n\nresponse is \n\n%s' % ([(str(i[0])+' -> '+str(i[1])) for i in resp.getheaders()], resp.read())
         except:    
             return sys.exc_info()
         finally:
