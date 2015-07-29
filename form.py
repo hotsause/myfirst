9bdfce1d31523126ab34efa5a45db76021a3d5bf
hotsause <qnmnp-github@yahoo.com>
2015-07-29 16:12:29 +0800

    update

--- form.py
+++ form.py
@@ -9,7 +9,15 @@ urls = (
         # '/(.*)','fun'
         )
 HC = httplib.HTTPConnection     
-heaD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0'}  
+heaD = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept encoding':'gzip, deflate',
+        'Accept language':'en-US,en;q=0.5',
+        'Connection':'keep-alive',
+        'Referer':'https://www.google.com',
+        'DNT':'1',
+        'Cache-Control':'max-age=0'
+        }  
 
 class fun():
     def GET(self):
@@ -36,11 +44,11 @@ class fun1():
             addr = addrresp = addr[7:]
         try:
             conn = HC(addr,timeout=100)
-            conn.request('get','/',headers = heaD)
+            conn.request('GET','/',headers = heaD)
             resp = conn.getresponse() 
             rend = web.template.render('templ/').form
-            cla.gloresp = (resp.getheaders(),resp.read())########baidu return badstatusline
-            return rend(cla.gloresp[0],cla.gloresp[1])###########global response, transfer to showresp.py
+            cla.gloresp = (resp.getheaders(),resp.read(),resp.version,resp.status,resp.reason)########baidu return badstatusline
+            return rend(cla.gloresp[0],cla.gloresp[1],cla.gloresp[2],cla.gloresp[3],cla.gloresp[4])###########global response, transfer to showresp.py
         except:    
             return sys.exc_info(),addr
         finally:
